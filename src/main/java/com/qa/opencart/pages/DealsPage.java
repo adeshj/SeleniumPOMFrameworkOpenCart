b694ddbdc8c551c2ddeed072d8c442a99d2bc181
Adesh Jain <adesh.jain@blueconchtech.com>
2022-09-01 01:57:26 +0530

    added pages

---
+++ src/main/java/com/qa/opencart/pages/DealsPage.java
@@ -0,0 +1,9 @@
+package com.qa.opencart.pages;
+
+public class DealsPage {
+	
+	public void dealMethod() {
+		System.out.println("deal method....");
+	}
+
+}
--- src/main/java/com/qa/opencart/pages/ProfilePage.java
+++ src/main/java/com/qa/opencart/pages/ProfilePage.java
@@ -1,6 +1,9 @@
-public class ProfilePage{
- 
-  ProfilePage(){
-  }
-  
+package com.qa.opencart.pages;
+
+public class ProfilePage {
+
+	ProfilePage() {
+		System.out.println("added profile page......");
+	}
+
 }
